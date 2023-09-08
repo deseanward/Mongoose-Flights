10d797cba0e60c64c168589559dce3fb5b18e380
De Sean Ward <d.ward.chi@gmail.com>
2023-09-07 23:20:41 -0500

    PORT variable fix for ENV in production

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Flight = require("./models/flight");
 const methodOverride = require("method-override");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // ***** Configure the App ***** //
 app.set("view engine", "jsx");
